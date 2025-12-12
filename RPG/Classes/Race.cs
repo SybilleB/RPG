5ce10d7c019ad023902c839ed46f775844de90d8
SybilleB <sybille.brassier@supdevinci-edu.fr>
2025-12-12 16:08:29 +0100

    modification du dépôt
    Ajout du .sln, du .gitignore avec .idea et du readme

--- RPG/Classes/Race.cs
+++ RPG/Classes/Race.cs
@@ -2,5 +2,18 @@ namespace RPG.Classes;
 
 public class Race
 {
+    public string nom;
+    public int poids;
+    public int pv;
+
+    public Race() {
+        pv = 100;
+    }
+
+    public Race(string nom, int poids) {
+        this.nom = nom;
+        this.poids = poids;
+        pv = 100;
+    }
     
 }
